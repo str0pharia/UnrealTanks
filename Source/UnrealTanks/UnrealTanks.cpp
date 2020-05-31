6f19bd8d7f4ab8fb1d08cee5ff4be22e745ede6e
Lukas Holmberg <lukas.adrian.holmberg@gmail.com>
2020-05-31 12:43:07 +0200

    Unreal project files

---
+++ Source/UnrealTanks/UnrealTanks.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealTanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealTanks, "UnrealTanks" );
